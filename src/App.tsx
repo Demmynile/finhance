541504bb4784819d8266a4ba79fa0a6455023573
Ademola <ademola@ademolas-mbp.home>
2024-06-30 09:23:47 +0100

    adding the navigation bar feature and setting up the foundations of the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,30 @@
-import { createTheme ,  Box } from '@mui/material'
-import {useMemo } from 'react'
-import { themeSettings } from './theme'
-import {CssBaseline , ThemeProvider} from '@mui/material'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './pages/navbar'
+import { createTheme, Box } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./pages/navbar";
+import Dashboard from "@/pages/dashboard";
 
 function App() {
- const theme = useMemo(() => createTheme(themeSettings) , [])
- 
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
   return (
-   
-      <div className='app'>
-        <BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Box
-            width = "100%"
-            height="100%"
-            padding="1rem 2rem 4rem 2rem"
-          >
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path = '/' element={<div>dashboard page</div>} />
-              <Route path = '/predictions' element = {<div>prediction page</div>} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<div>prediction page</div>} />
             </Routes>
-
           </Box>
-         </ThemeProvider>
-        </BrowserRouter>
-         
-      </div>
-
-  )
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,36 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 
 type Props = {};
 
+const gridTemplate = `
+ "a b c"
+ "a b c"
+ "a b c"
+ "a b f"
+ "d e f"
+ "d e f"
+ "d h i"
+ "g h i"
+ "g h j"
+ "g h j"
+`;
+
 const Dashboard = (props: Props) => {
-  return <Box>Dashboard</Box>;
+  const { palette } = useTheme();
+
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={{
+        gridTemplateAreas: gridTemplate,
+      }}
+    >
+      Dashboard
+    </Box>
+  );
 };
 
 export default Dashboard;
